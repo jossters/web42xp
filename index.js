3cd13ae326e95ecba6c3377c6a37f6e4320198ea
Johnny Murillo <jossters@gmail.com>
2021-06-03 10:15:50 -0700

    wip

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hey')
+require('dotenv').config()
+const express = require('express');
+const server = express()
+
+console.log(process.env.USER);
+
+const PORT = process.env.PORT || 5000
+
+server.get('/api', (req, res) => {
+    res.json({message: `${process.env.COHORT}`})
+})
+
+server.listen(PORT, () => {
+console.log(`listening on ${PORT}`)
+})
