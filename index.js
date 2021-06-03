e4e67a8f92f5c776911b874175616c0d4115cdc3
Johnny Murillo <jossters@gmail.com>
2021-06-03 10:59:51 -0700

    wip

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors')
+const path = require('path')
+
 const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(express.static(path.join(__dirname, 'client/build'))) //static assets
 
 console.log(process.env.USER);
 
 const PORT = process.env.PORT || 5000
+
+server.get('/', (req, res) => {
+    //sending back index.html
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 
 server.get('/api', (req, res) => {
     res.json({message: `${process.env.COHORT}`})
